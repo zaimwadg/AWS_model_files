e3df9d5439af353c71771139aee02f2141c77d68
zaimwadg <nacer.zaimwadghiri@epfl.ch>
2017-02-09 22:22:46 +0100

    new file:   script_to_install.sh

---
+++ script_to_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+mv -v /home/ubuntu/AWS_model_files/* /home/ubuntu
+
+rm -rf /home/ubuntu/AWS_model_files
+
+wget https://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh
+
+expect install_anaconda
+
+source .bashrc
+
+jupyter notebook --generate-config
+
+cd /home/ubuntu/.jupyter/
+
+echo -e "c = get_config()\nc.IPKernelApp.pylab = 'inline'\nc.NotebookApp.ip = '*'\nc.NotebookApp.open_browser = False\nc.NotebookApp.port = 8888" >> jupyter_notebook_config.py
+
+cd ..
+
+expect python_3
